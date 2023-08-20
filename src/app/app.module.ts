b4fa285bd787ee74f4f8b20212e9c3bccc2a0648
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2023-08-20 10:23:12 +0100

    Covalent and material config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CovalentDynamicFormsModule} from "@covalent/dynamic-forms";
+import {CovalentLayoutModule} from "@covalent/core/layout";
+import {CovalentMarkdownModule} from "@covalent/markdown";
+import {CovalentHighlightModule} from "@covalent/highlight";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CovalentLayoutModule,
+    // (optional) Additional Covalent Modules imports
+    CovalentDynamicFormsModule,
+    CovalentHighlightModule,
+    CovalentMarkdownModule
+    ,
   ],
   providers: [],
   bootstrap: [AppComponent]
